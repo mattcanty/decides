72cfa62d29723333efbf204fc4b54cc66330c2f6
Matt Canty <matthewcanty@gmail.com>
2016-02-23 11:05:55 +0000

    Return git commit hash from /version

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public')).listen(process.env.PORT, process.env.IP);
+app.get('/version', function(req, res){
+  res.send(process.env.SOURCE_VERSION);
+});
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT, process.env.IP);
 
 console.log('Server started.');
